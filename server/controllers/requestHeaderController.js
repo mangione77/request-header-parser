896f9fe465ad8bb36ec1a006a1f8206c2ee64575
Pablo Mangione <mangione.mkt@gmail.com>
2017-12-14 20:35:02 +0100

    Added controller

--- server/controllers/requestHeaderController.js
+++ server/controllers/requestHeaderController.js
@@ -1,7 +1,20 @@
 const requestHeaderController = {}
 
 requestHeaderController.getData = (req,res) => {
-	console.log(req.headers)
+	let ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress
+
+     let language = req.headers['accept-language'].split(',')[0]
+     let user_agent = req.headers['user-agent'].split(') ')[0].split(' (')[1]
+
+	res.status(200).json({
+		ip:ip,
+		language:language,
+		user_agent:user_agent
+		
+	})
 }
 
 module.exports = requestHeaderController
